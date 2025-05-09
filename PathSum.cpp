b75e1380c99e1069318ad2d7a47551f4b6e19dde
Sizos1 <daaguilarc@unal.edu.co>
2025-05-09 01:28:36 -0500

    hola

--- PathSum.cpp
+++ PathSum.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include <string>
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+#include "TreeNode.cpp"
 
 void search(TreeNode *root, int targetSum, int sum, bool &found)
 {
