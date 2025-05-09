3999a8ace8935ff1434116ad709b4d177e092947
Sizos1 <daaguilarc@unal.edu.co>
2025-05-09 01:22:28 -0500

    PathSum

---
+++ PathSum.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void search(TreeNode *root, int targetSum, int sum, bool &found)
+{
+    if (root == nullptr)
+        return;
+    sum += root->val;
+    if (root->left == nullptr && root->right == nullptr)
+    {
+        if (sum == targetSum)
+        {
+            found = true;
+        }
+    }
+    search(root->left, targetSum, sum, found);
+    search(root->right, targetSum, sum, found);
+}
+
+bool hasPathSum(TreeNode *root, int targetSum)
+{
+    bool found = false;
+    search(root, targetSum, 0, found);
+    return found;
+}
+int main()
+{
+    TreeNode *root = new TreeNode(5);
+    // Level 2
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+    // Level 3
+    root->left->left = new TreeNode(11);
+    root->right->left = new TreeNode(13);
+    root->right->right = new TreeNode(4);
+    // Level 4
+    root->left->left->left = new TreeNode(7);
+    root->left->left->right = new TreeNode(2);
+    root->right->right->right = new TreeNode(1);
+    std::cout << "Has path sum 10: " << (hasPathSum(root, 10) ? "true" : "false") << std::endl;
+
+    return 0;
+}
