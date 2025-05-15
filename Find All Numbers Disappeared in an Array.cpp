53ccb495ab94c2d926f103e37ecce31a4078503f
Sizos1 <daaguilarc@unal.edu.co>
2025-05-15 01:32:32 -0500

    Hay una solución con espacio constante pero no la hice

---
+++ Find All Numbers Disappeared in an Array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+vector<int> findDisappearedNumbers(vector<int> &nums)
+{
+    vector<int> result;
+    unordered_set<int> set;
+    for (int num : nums)
+    {
+        set.insert(num);
+    }
+    for (int num = 1; num < nums.size() + 1; num++)
+    {
+        if (set.find(num) == set.end())
+        {
+            result.push_back(num);
+        }
+    }
+    return result;
+}
+
+// No supe que hacer con la solución de espacio constante nagh
