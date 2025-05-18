35d5228d1a76bae5283329ebdfa014a5ae9ba27b
Sizos1 <daaguilarc@unal.edu.co>
2025-05-18 00:58:42 -0500

    .

--- Tests.cpp
+++ Tests.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 
 using namespace std;
 
+vector<unsigned long long int> fib(int n)
+{
+    vector<unsigned long long int> fib = {0, 1};
+    for (int i = 2; i <= n; i++)
+    {
+        fib.push_back(fib[i - 1] + fib[i - 2]);
+    }
+    return fib;
+}
+
 int main()
 {
-    cout << 9 / 10;
+    int n = 3 % 3;
+    cout << n;
     return 0;
 }
